ed334bd83c81ffd64041324fd51822a8f2fe52e4
PattyL <patty_ailinda@yahoo.com>
2016-03-09 20:08:34 -0800

    add Jquery
    add Jquery follow class instruction

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+// sticky navbar
+$(document).ready(function () {
+	$(".navbar").sticky({
+		topSpacing: 0
+	});
+});
+
+// smooth scroll
+$(document).ready(function () {
+	$('.scrollto a[href*=#]:not([href=#])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 50
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
+// scroll to top
+$(document).ready(function(){
+	
+    //Check to see if the window is top if not then display button
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 800) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
+
+    //Click event to scroll to top
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+	
+});
+
+// parallax
+$(function() {
+    $.stellar();
+});
+
+// main flex slider
+$(window).load(function() {
+    $('.main-flex-slider').flexslider({
+        slideshowSpeed: 5000,
+        directionNav: false,
+        animation: "fade",
+        controlNav: false
+    });
+});
+
+// owl carousel
+$(document).ready(function() {
+    $("#testi-carousel").owlCarousel({
+        items: 1,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true
+    });
+});
