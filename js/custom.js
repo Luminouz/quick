991024a82cd74b39ad2fb6fdc51ab87783e1efdb
PattyL <patty_ailinda@yahoo.com>
2016-04-06 17:59:44 -0700

    add custom.js files

--- js/custom.js
+++ js/custom.js
@@ -25,20 +25,20 @@ $(document).ready(function () {
 // scroll to top
 $(document).ready(function(){
 	
-    //Check to see if the window is top if not then display button
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 800) {
-                $('.scrollToTop').fadeIn();
-            } else {
-                $('.scrollToTop').fadeOut();
-            }
-        });
-
-    //Click event to scroll to top
-        $('.scrollToTop').click(function(){
-            $('html, body').animate({scrollTop : 0},800);
-            return false;
-        });
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 800) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 	
 });
 
@@ -67,3 +67,29 @@ $(document).ready(function() {
 		autoplayHoverPause:true
     });
 });
+
+// counter up
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 800
+    });
+});
+
+// magnific popup
+jQuery(document).ready(function($) {
+	$('.show-image').magnificPopup({
+		type: 'image'
+	});
+});
+
+// wow animate
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
